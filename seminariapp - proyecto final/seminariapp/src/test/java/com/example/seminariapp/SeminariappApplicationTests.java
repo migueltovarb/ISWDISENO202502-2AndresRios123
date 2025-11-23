3bc4fdacb4b2519b6d97c237b1e5ad14ae11c9fe
AndresRios123 <andresc.rios@campusucc.edu.com>
2025-11-23 00:07:27 -0500

    archivo base proyecto final agregado

---
+++ seminariapp - proyecto final/seminariapp/src/test/java/com/example/seminariapp/SeminariappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seminariapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminariappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
